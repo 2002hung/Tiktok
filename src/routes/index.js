0f8b8fb54a43dad69576723607a748002e6c17eb
2002Hung <hungchu101@gmail.com>
2022-12-25 10:18:47 +0700

    Cấu hình Router/Layout

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import { HeaderOnly } from '../component/Layout';
+import Following from '~/pages/Following';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
